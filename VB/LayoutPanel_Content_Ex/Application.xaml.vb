a534a6778143301b5a39db11431b952a0d14e2f3
DevExpressExampleBot <fakeEmail>
2021-09-06 17:07:11 +0300

    Source auto update [skip ci]

--- VB/LayoutPanel_Content_Ex/Application.xaml.vb
+++ VB/LayoutPanel_Content_Ex/Application.xaml.vb
@@ -1,8 +1,8 @@
 ﻿Imports System.Windows
 
 Namespace LayoutPanel_Content_Ex
-    Partial Public Class App
-        Inherits Application
+	Partial Public Class App
+		Inherits Application
 
-    End Class
+	End Class
 End Namespace
